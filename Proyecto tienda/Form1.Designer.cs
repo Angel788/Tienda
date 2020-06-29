3d64a126ec72e7bc92d64a20b35c938926b3aab2
Angel788 <angel-yahel@outlook.com>
2020-06-29 15:46:22 -0500

    Se agrego el logo

--- Proyecto tienda/Form1.Designer.cs
+++ Proyecto tienda/Form1.Designer.cs
@@ -70,10 +70,11 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Image = global::Proyecto_tienda.Properties.Resources.descarga__1_1;
-            this.pictureBox1.Location = new System.Drawing.Point(24, 21);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 78);
+            this.pictureBox1.Size = new System.Drawing.Size(182, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
